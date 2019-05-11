bce1c9070c0f0dcd11aeff5199b6e17c12787d2e
Matvii Karpenko <mtw.karp@gmail.com>
2019-05-12 00:22:00 +0200

    showing all films script ready

--- client/src/components/MoviesInfoBlock.jsx
+++ client/src/components/MoviesInfoBlock.jsx
@@ -1,33 +1,53 @@
 import React from 'react' 
-import avengers from '../avengers.jpg'
+
 //import Cinema from './actions/fetchMovies'
 import FetchMovies from './actions/fetchMovies(WOJTEK)'
 
 
 
 class MoviesInfoBlock extends React.Component {
-
+    state = {
+        movieArr: [{id: null, vote_average:null, title: null, poster_path: null}]
+    }
+    
+    componentDidMount() {
+        FetchMovies().then(a => { this.setState({movieArr: a}) })
+    }
     render () {
-        console.log(FetchMovies().then(a => { console.log(a) }))
+        FetchMovies().then(a => { console.log({a}) })
         return (
             <div className="moviesHolder">
-        
-                <div className="particularMovie">
-                    <img src={avengers} alt="Movie poster"/>
+                {this.state.movieArr.map(m => { return (    
+                <div className="particularMovie" key={m.id}>
+                <img alt={m.title} src={`http://image.tmdb.org/t/p/w185/${m.poster_path}`} />
                     <div className="movieInfo">
-                    <p className="movieName">Avengers</p>
-                    <p className="genre">Thriller, comedy</p>
-                    <p className="durationAndCountry">120 min, USA</p>
+                        <p className="movieName">{m.title}</p>
+                        <p className="genre">Thriller, comedy</p>
+                        <p className="durationAndCountry">120 min, USA</p>
                     </div>
                     <div className="ratingBookmark">
-                    <p className="ratingHolder">
-                    <i className="tiny material-icons">star_border</i>5.9</p>
+                        <p className="ratingHolder">
+                         <i className="tiny material-icons">star_border</i>{m.vote_average}</p>
                     </div>
                 </div>
+                )})}
             </div>
         )
     }
 }
 
 export default MoviesInfoBlock
-//return (<div>{array.map(m => { return (<p>{m.title}</p>)})}</div>)
+
+
+    // < div className = "particularMovie" >
+    //     <img src={avengers} alt="Movie poster" />
+    //     <div className="movieInfo">
+    //         <p className="movieName">Avengers</p>
+    //         <p className="genre">Thriller, comedy</p>
+    //         <p className="durationAndCountry">120 min, USA</p>
+    //     </div>
+    //     <div className="ratingBookmark">
+    //         <p className="ratingHolder">
+    //             <i className="tiny material-icons">star_border</i>5.9</p>
+    //     </div>
+    //             </div >
